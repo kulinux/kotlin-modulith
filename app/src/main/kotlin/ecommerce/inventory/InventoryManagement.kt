414e9d2a576d9745a1176ba6ae67f663714ed2e3
Francisco Jose Benitez Lazaro <paco.benitez.lazaro@gmail.com>
2024-01-03 07:27:11 +0100

    fix modulith test

--- app/src/main/kotlin/ecommerce/inventory/InventoryManagement.kt
+++ app/src/main/kotlin/ecommerce/inventory/InventoryManagement.kt
@@ -1,11 +1,7 @@
 package ecommerce.inventory
 
-import ecommerce.order.internal.InternalOrderManagement
 import org.springframework.stereotype.Service
 
 @Service
 class InventoryManagement {
-    fun method() {
-        val internal = InternalOrderManagement()
-    }
 }
